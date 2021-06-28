8b07d270b172e9c9c2095a863533aa51cd66e431
Brenex000006 <85696049+Brenex000006@users.noreply.github.com>
2021-06-28 14:31:47 -0300

    Add files via upload

---
+++ checkpoint1.js
@@ -0,0 +1,246 @@
+//Aula 11 - 28/06/2021
+// checkpoint.js
+// Checkpoint 1 - Microondas
+
+let menu = "Feijão"
+
+switch(menu) {
+    case "Pipoca":
+        console.log("Tempo mínimo necssário para preparo: 10secs")
+break;
+    case "Macarrão":
+        console.log("Tempo mínimo necssário para preparo: 8secs")
+break;
+    case "Carne":
+        console.log("Tempo mínimo necssário para preparo: 15secs")
+break;
+    case "Feijão":
+        console.log("Tempo mínimo necssário para preparo: 12secs")
+break;
+    case "Brigadeiro":
+        console.log("Tempo mínimo necssário para preparo: 8secs")
+break;
+default:
+    console.log("Prato Inexistente")
+}; 
+
+let tempo_pipoca = "0"
+
+switch(tempo_pipoca) {
+    case "9":
+    case "8":
+    case "7":
+    case "6":
+    case "5":
+    case "4":
+    case "3":
+    case "2":
+    case "1":
+        console.log("Tempo Insuficiente")
+break;
+    case "29":
+    case "28":
+    case "27":
+    case "26":
+    case "25":
+    case "24":
+    case "23":
+    case "22":
+    case "21":
+    case "20":
+        console.log("A comida quiemou")
+break;
+    case "39":
+    case "38":
+    case "37":
+    case "36":
+    case "35":
+    case "34":
+    case "33":
+    case "32":
+    case "31":
+    case "30":
+        console.log("Kabumm")
+break;
+case "0":
+    console.log("")
+break;
+default:
+    console.log("Prato pronto, bom apetite!!!")
+}; 
+
+let tempo_macarrao = "0"
+
+switch(tempo_macarrao) {
+    case "7":
+    case "6":
+    case "5":
+    case "4":
+    case "3":
+    case "2":
+    case "1":
+        console.log("Tempo Insuficiente")
+break;   
+    case "23":
+    case "22":
+    case "21":
+    case "20":
+    case "19":
+    case "18":
+    case "17":
+    case "16":
+        console.log("A comida quiemou")
+break;
+    case "30":
+    case "29":
+    case "28":
+    case "27":
+    case "26":
+    case "25":
+    case "24":
+        console.log("Kabumm")
+break;
+case "0":
+    console.log("")
+break;
+default:
+    console.log("Prato pronto, bom apetite!!!")
+}; 
+
+let tempo_carne = "0"
+
+switch(tempo_carne) {
+    case "14":
+    case "13":
+    case "12":
+    case "11":
+    case "10":
+    case "9":
+    case "8":
+    case "7":
+    case "6":
+    case "5":
+    case "4":
+    case "3":
+    case "2":
+    case "1":
+        console.log("Tempo Insuficiente")
+break;
+    case "44":
+    case "43":
+    case "42":
+    case "41":
+    case "40":
+    case "39":
+    case "38":
+    case "37":
+    case "36":
+    case "35":
+    case "34":
+    case "33":
+    case "32":
+    case "31":
+    case "30":
+        console.log("A comida quiemou")
+break;
+    case "50":
+    case "49":
+    case "48":
+    case "47":
+    case "46":
+    case "45":
+        console.log("Kabumm")
+break;
+case "0":
+    console.log("")
+break;
+default:
+    console.log("Prato pronto, bom apetite!!!")
+};
+
+let tempo_feijao = "12"
+
+switch(tempo_feijao) {
+    case "11":
+    case "10":
+    case "9":
+    case "8":
+    case "7":
+    case "6":
+    case "5":
+    case "4":
+    case "3":
+    case "2":
+    case "1":
+        console.log("Tempo Insuficiente")
+break;
+    case "35":
+    case "34":
+    case "33":
+    case "32":
+    case "31":
+    case "30":
+    case "29":
+    case "28":
+    case "27":
+    case "26":
+    case "25":
+    case "24":
+        console.log("A comida quiemou")
+break;
+    case "45":
+    case "44":
+    case "43":
+    case "42":
+    case "41":
+    case "40":
+    case "39":
+    case "38":
+    case "37":
+    case "36":
+        console.log("Kabumm")
+break;
+    case "0":
+        console.log("")
+break;
+default:
+    console.log("Prato pronto, bom apetite!!!")
+};
+
+let tempo_brigadeiro = "0"
+
+switch(tempo_brigadeiro) {
+    case "7":
+    case "6":
+    case "5":
+    case "4":
+    case "3":
+    case "2":
+    case "1":
+        console.log("Tempo Insuficiente")
+break;   
+    case "23":
+    case "22":
+    case "21":
+    case "20":
+    case "19":
+    case "18":
+    case "17":
+    case "16":
+        console.log("A comida quiemou")
+break;
+    case "30":
+    case "29":
+    case "28":
+    case "27":
+    case "26":
+    case "25":
+    case "24":
+        console.log("Kabumm")
+break;
+case "0":
+    console.log("")
+break;
+default:
+    console.log("Prato pronto, bom apetite!!!")
+};
